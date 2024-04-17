6f686cea7dbaafe3c4f0bb360362d4794a7521dc
salem <salemadamscs@gmail.com>
2024-04-17 13:54:19 -0400

    - Made data model for calculator operations

---
+++ CalculatorWebServerApp/Components/Data/CalculatorData.cs
@@ -0,0 +1,15 @@
+﻿namespace CalculatorWebServerApp.Components.Data;
+
+public class CalculatorData
+{
+    public CalculatorData(string inputA, string inputB, double result)
+    {
+        this.inputA = inputA;
+        this.inputB = inputB;
+        this.result = result;
+    }
+
+    public string inputA { get; set; }
+    public string inputB { get; set; }
+    public double result { get; set; }
+}
